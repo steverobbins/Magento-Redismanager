9ce9bf1d5831b5d5d785ce7427019dcfde89fdb1
Steve Robbins <steven.j.robbins@gmail.com>
2014-07-19 11:44:32 -0700

    code clean up

--- app/code/community/Steverobbins/Redismanager/Helper/Data.php
+++ app/code/community/Steverobbins/Redismanager/Helper/Data.php
@@ -4,16 +4,19 @@ class Steverobbins_Redismanager_Helper_Data extends Mage_Core_Helper_Abstract
 {
     const XML_PATH_DEFAULT_SECTION = 'redismanager';
     const XML_PATH_DEFAULT_GROUP   = 'settings';
+
     /**
      * Config cache
      * @var array
      */
     private $_config = array();
+
     /**
      * Services cache
      * @var array
      */
     private $_services;
+
     /**
      * Config getter
      * 
@@ -33,6 +36,7 @@ class Steverobbins_Redismanager_Helper_Data extends Mage_Core_Helper_Abstract
         }
         return $this->_config[$path];
     }
+
     /**
      * Fetch all redis services
      *
@@ -44,37 +48,30 @@ class Steverobbins_Redismanager_Helper_Data extends Mage_Core_Helper_Abstract
             if ($this->getConfig('auto')) {
                 $this->_services = array();
                 $config = Mage::app()->getConfig();
-                // get cache
-                $node = $config->getXpath('global/cache[1]');
-                if ((string)$node[0]->backend == 'Cm_Cache_Backend_Redis') {
-                    $this->_services[] = array(
-                        'name'     => $this->__('Cache'),
-                        'host'     => (string)$node[0]->backend_options->server,
-                        'port'     => (string)$node[0]->backend_options->port,
-                        'password' => (string)$node[0]->backend_options->password,
-                        'db'       => (string)$node[0]->backend_options->database
-                    );
-                }
-                // get fpc
-                $node = $config->getXpath('global/full_page_cache[1]');
-                if ((string)$node[0]->backend == 'Cm_Cache_Backend_Redis') {
-                    $this->_services[] = array(
-                        'name'     => $this->__('Page Cache'),
-                        'host'     => (string)$node[0]->backend_options->server,
-                        'port'     => (string)$node[0]->backend_options->port,
-                        'password' => (string)$node[0]->backend_options->password,
-                        'db'       => (string)$node[0]->backend_options->database
-                    );
+                foreach (array('cache', 'full_page_cache') as $cacheType) {
+                    $node = $config->getXpath('global/' . $cacheType . '[1]');
+                    if (in_array((string)$node[0]->backend, array(
+                        'Cm_Cache_Backend_Redis',
+                        'Mage_Cache_Backend_Redis'
+                    ))) {
+                        $this->_services[] = $this->_buildServiceArray(
+                            $this->__('Cache'),
+                            $node[0]->backend_options->server,
+                            $node[0]->backend_options->port,
+                            $node[0]->backend_options->password,
+                            $node[0]->backend_options->database
+                        );
+                    }
                 }
                 // get session
                 $node = $config->getXpath('global/redis_session');
                 if ($node) {
-                    $this->_services[] = array(
-                        'name'     => $this->__('Session'),
-                        'host'     => (string)$node[0]->host,
-                        'port'     => (string)$node[0]->port,
-                        'password' => (string)$node[0]->password,
-                        'db'       => (string)$node[0]->db
+                    $this->_services[] = $this->_buildServiceArray(
+                        $this->__('Session'),
+                        $node[0]->host,
+                        $node[0]->port,
+                        $node[0]->password,
+                        $node[0]->db
                     );
                 }
             }
@@ -83,5 +80,55 @@ class Steverobbins_Redismanager_Helper_Data extends Mage_Core_Helper_Abstract
             }
         }
         return $this->_services;
+    }
+
+    /**
+     * Assign values with casting
+     * 
+     * @param  string $name
+     * @param  string $host
+     * @param  string $port
+     * @param  string $pass
+     * @param  string $db
+     * @return array
+     */
+    protected function _buildServiceArray($name, $host, $port, $pass, $db)
+    {
+        return array(
+            'name'     => $name,
+            'host'     => (string)$host,
+            'port'     => (string)$port,
+            'password' => (string)$pass,
+            'db'       => (string)$db
+        );
+    }
+    
+    /**
+     * Get Redis client
+     * 
+     * @param  string $host
+     * @param  string $port
+     * @param  string $pass
+     * @param  string $db
+     * @return Mage_Cache_Backend_Redis|Cm_Cache_Backend_Redis
+     */
+    public function getRedisInstance($host, $port, $pass, $db)
+    {
+        if (class_exists('Mage_Cache_Backend_Redis')) {
+            return new Mage_Cache_Backend_Redis(array(
+                'server'   => $host,
+                'port'     => $port,
+                'password' => $pass,
+                'database' => $db
+            ));
+        } else if (class_exists('Cm_Cache_Backend_Redis')) {
+            return new Cm_Cache_Backend_Redis(array(
+                'server'   => $host,
+                'port'     => $port,
+                'password' => $pass,
+                'database' => $db
+            ));
+        }
+        return false;
     }
 }
