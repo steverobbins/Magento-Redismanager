edfbf3f25a29a2ab55967c1f2c146af895134330
Steve Robbins <steven.j.robbins@gmail.com>
2014-07-19 20:55:01 -0700

    doc block all the things

--- app/code/community/Steverobbins/Redismanager/Helper/Data.php
+++ app/code/community/Steverobbins/Redismanager/Helper/Data.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Redis Management Module
+ * 
+ * @category   Steverobbins
+ * @package    Steverobbins_Redismanager
+ * @author     Steve Robbins <steven.j.robbins@gmail.com>
+ * @copyright  Copyright (c) 2014 Steve Robbins (https://github.com/steverobbins)
+ * @license    http://creativecommons.org/licenses/by/3.0/deed.en_US Creative Commons Attribution 3.0 Unported License
+ */
 
 class Steverobbins_Redismanager_Helper_Data extends Mage_Core_Helper_Abstract
 {
--- app/code/community/Steverobbins/Redismanager/Model/Backend/Redis/Cm.php
+++ app/code/community/Steverobbins/Redismanager/Model/Backend/Redis/Cm.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Redis Management Module
+ * 
+ * @category   Steverobbins
+ * @package    Steverobbins_Redismanager
+ * @author     Steve Robbins <steven.j.robbins@gmail.com>
+ * @copyright  Copyright (c) 2014 Steve Robbins (https://github.com/steverobbins)
+ * @license    http://creativecommons.org/licenses/by/3.0/deed.en_US Creative Commons Attribution 3.0 Unported License
+ */
 
 class Steverobbins_Redismanager_Model_Backend_Redis_Cm extends Cm_Cache_Backend_Redis
 {
