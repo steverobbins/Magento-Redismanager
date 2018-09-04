4c8e96921b12a0a6b516a84ac997cda856d90d0b
Henry van Megen <hvanmegen@hostingxs.nl>
2018-09-04 12:59:19 +0200

    Fixed files to meet standards

--- src/app/code/community/Steverobbins/Redismanager/Model/Observer.php
+++ src/app/code/community/Steverobbins/Redismanager/Model/Observer.php
@@ -1,5 +1,27 @@
 <?php
+/**
+ * Redis Management Module
+ *
+ * PHP Version 5
+ *
+ * @category  Steverobbins
+ * @package   Steverobbins_Redismanager
+ * @author    Steve Robbins <steven.j.robbins@gmail.com>
+ * @copyright 2014 Steve Robbins
+ * @license   http://creativecommons.org/licenses/by/3.0/deed.en_US Creative Commons Attribution 3.0 Unported License
+ * @link      https://github.com/steverobbins/Magento-Redismanager
+ */
 
+/**
+ * Redis manager observer class
+ *
+ * @category  Steverobbins
+ * @package   Steverobbins_Redismanager
+ * @author    Henry van Megen <h.van.megen@gmail.com>
+ * @copyright 2018 Henry van Megen
+ * @license   http://creativecommons.org/licenses/by/3.0/deed.en_US Creative Commons Attribution 3.0 Unported License
+ * @link      https://github.com/steverobbins/Magento-Redismanager
+ */
 class Steverobbins_Redismanager_Model_Observer
 {
     /**
@@ -10,11 +32,13 @@ class Steverobbins_Redismanager_Model_Observer
     protected $_helper;
 
     /**
+     * Event observer for adminhtml_cache_flush_system event
+     *
      * @param Varien_Event_Observer $observer
      *
      * @return $this
      */
-    public function adminhtml_cache_flush_system(Varien_Event_Observer $observer)
+    public function cacheFlushSystem(Varien_Event_Observer $observer)
     {
         try {
             $this->_getHelper()->flushAllByObserver();
@@ -25,17 +49,19 @@ class Steverobbins_Redismanager_Model_Observer
     }
 
     /**
+     * Event observer for adminhtml_cache_flush_all event
+     *
      * @param Varien_Event_Observer $observer
      *
      * @return $this
      */
-    public function adminhtml_cache_flush_all(Varien_Event_Observer $observer)
+    public function cacheFlushAll(Varien_Event_Observer $observer)
     {
         try {
             $this->_getHelper()->flushAllByObserver();
         } catch (Exception $e) {
             Mage::logException($e);
-        } 
+        }
         return $this;
     }
 
@@ -44,11 +70,11 @@ class Steverobbins_Redismanager_Model_Observer
      *
      * @return Steverobbins_Redismanager_Helper_Data
      */
-     protected function _getHelper()
-     {
-         if (is_null($this->_helper)) {
-             $this->_helper = Mage::helper('redismanager');
-         }
-         return $this->_helper;
-     }
+    protected function _getHelper()
+    {
+        if (is_null($this->_helper)) {
+            $this->_helper = Mage::helper('redismanager');
+        }
+        return $this->_helper;
+    }
 }
